86f259080b34f6fc1bd34c8d5ad198cc7ab502de
sahanbimsara <sahanbimsara2060@gmail.com>
2025-06-07 11:50:31 +0530

    project Initialize

---
+++ src/main/java/edu/icet/ecom/model/Student.java
@@ -0,0 +1,17 @@
+package edu.icet.ecom.model;
+import lombok.*;
+
+@Getter
+@Setter
+public class Student {
+
+    private String id;
+
+    private String name;
+
+    private String address;
+
+    private Integer age;
+
+    private String email;
+}
