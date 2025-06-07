091edd42f4cd27bf5b60515c9087b0bfabce9988
sahanbimsara <sahanbimsara2060@gmail.com>
2025-06-07 14:02:16 +0530

    yml file cretea database and databse table

--- src/main/java/edu/icet/ecom/model/entity/StudentEntity.java
+++ src/main/java/edu/icet/ecom/model/entity/StudentEntity.java
@@ -1,11 +1,22 @@
 package edu.icet.ecom.model.entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Students")
 public class StudentEntity {
 
+    @Id
     private long id;
 
     private String name;
